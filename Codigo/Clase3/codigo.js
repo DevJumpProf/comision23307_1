85f2b3188a44ada9de0744b9db7137ecc06c793b
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-03-08 00:05:41 -0300

    JS

---
+++ Codigo/Clase3/codigo.js
@@ -0,0 +1,55 @@
+// interacciones
+
+// alert("Hola Codo a Codo")
+
+// prompt ("Ingresa tu nombre")
+
+// confirm("queres ingresar?")
+
+
+// variables // tipos de datos.
+
+// let var const
+
+
+/* 
+let nombre = "Jose"; //"cadena de texto"  - STRING
+let apellido = "San Martin"
+let edad = 37;  // Numero - NUMBER
+let numero_favorito= "13
+let NumeroFavorito= "13"  //upper camel case  
+ //numeroFAvorito                  // lower camel case
+let estudia= true;  // Booleano - BOOLEAN
+let materias= ["javascript","node",12]  //Arreglo / matriz -ARRAY
+let finalizado = null;
+estudia= "no"
+let NumeroFAvorito= "14"   // case sensitive
+
+
+let nombre2 = prompt ("Decime tu nombre")
+alert("Hola " + nombre2 + " Estas en mi pagina")
+
+
+let num1 = Number(prompt ("Decime un numero"))
+let num2 = Number(prompt ("Decime otro numero"))
+
+document.write ("el resultado de tu suma es: " + (num1 + num2)); */
+
+
+/*  + - * /  % */
+
+
+let modulo =13546546547%2
+
+document.write (modulo)
+
+
+
+
+
+
+
+
+
+
+
