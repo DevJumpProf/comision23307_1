c0f929f19d8ce09d358b991095aa1e5ff6b64d8d
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-04-27 22:26:11 -0300

    firebase

---
+++ Codigo/Clase17/crud-react-firebase/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+   <Routes>
+    <Route path="/" element= "Show"/>
+    <Route path="/create" element= "Create"/>
+    <Route path="/edit/:id" element= "Edit"/>
+   </Routes>
+         </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
