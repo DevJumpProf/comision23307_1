db992b652253b4a0b0dbddffff2f01d0b6463b6a
Gabriel Muñoz <gabriel.munoz@arbusta.net>
2023-06-02 00:31:12 -0300

    MOVIES OK

--- Codigo/Clase24/movies-cac/src/components/Buscador.jsx
+++ Codigo/Clase24/movies-cac/src/components/Buscador.jsx
@@ -1,14 +1,35 @@
 import { FaSearch } from "react-icons/fa";
-import "./Buscador.css"
+import { useNavigate } from "react-router-dom"; // 4 importo useNavigate
+import { useState } from "react"; //7 importamos useState
+import "./Buscador.css";
 export const Buscador = () => {
+  const navigate = useNavigate(); //5 traigo la ejecucion y lo guaro en la variable
+  // 1 realiza la funcion que nos permita captura lo que ingresamos en el input del buscador. y lo envie a la url
+
+  /* 8 estados para capturar el input */
+  const [searchText, setSearchText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //3 mostrar que no se realiza el submit
+    navigate(`/?search=${searchText}`); //6 las preparo el useNavigate para enviar lo buscado a la url
+  };
+
   return (
-    <form className="buscadorContainer">
+    <form className="buscadorContainer" onSubmit={handleSubmit}>
+  
+      {/* 2 agregamos el evento */}
       <div className="buscadorBox">
-        <input 
-        className="buscadorInput"
-        type="text" 
+        <input
+          className="buscadorInput"
+          /*       9 valor del estado inicial */
+          value={searchText}
+          /*  10 trabajamos con el evento onChange capturando el cambio en el input */
+          onChange={(e) => setSearchText(e.target.value)}
+          type="text"
         />
-        <button type="submit" className="buscadorButton"><FaSearch/></button>
+        <button type="submit" className="buscadorButton">
+          <FaSearch />
+        </button>
       </div>
     </form>
   );
--- Codigo/Clase24/movies-cac/src/components/PeliculasGrid.jsx
+++ Codigo/Clase24/movies-cac/src/components/PeliculasGrid.jsx
@@ -1,22 +1,42 @@
 /* import peliculas from "./peliculas.json" */
 import {get} from "../utils/httpCliente.js"
 import { useState, useEffect } from "react"
+import { useLocation } from "react-router-dom"; // 11 importamos useLocation
 import { PeliculasCard } from "./PeliculasCard"
 import { Spinner } from "../components/Spinner.jsx";
 import "./PeliculasGrid.css"
 
 export const PeliculasGrid=()=>{
 
+/*     13 utilizamos este hook para tomar lo que viene por params del buscador */
+const useQuery = ()=>{
+  return  new URLSearchParams(useLocation().search)
+}
+//14 es guardar en la variable search lo que se está buscando
+ const query = useQuery()
+ const search = query.get("search")
+/*  console.log(search); */
+
+
+    //12 mostrar uso de useLocation()
+/*     const location = useLocation() */
+   /*  console.log(location);  */// vemos en el navegador
+ /*   console.log(location.search) */
 const [cargando,setCargando]= useState(true);
 const [peliculas,setPeliculas] = useState([])
    
 useEffect(()=>{
+    //15 realizamos un ternario , si hay busqueda, hace un llamado al enpoint de busqueda, sino al de peliculas
+    const searchUrl = search 
+    ? "/search/movie?query="+search 
+    :"/discover/movie"
+
     setCargando(true)
-get("/discover/movie").then((data)=>{
+get(searchUrl).then((data)=>{  // 16 agrego searchUrl
     setPeliculas(data.results);
     setCargando(false)
 })
-},[])
+},[search]) // 17 agrego search
 
 if(cargando){
     return <Spinner/>
